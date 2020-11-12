8721716ae67c93a99de489f973e9631b81e85a4e
Derek <Derek@DEREK>
2020-11-12 23:18:32 +0900

    Solved 삼각달팽이 problem

--- src/main/java/week1/question1/Solution.java
+++ src/main/java/week1/question1/Solution.java
@@ -1,12 +1,63 @@
 /*
-  https://programmers.co.kr/learn/courses/30/lessons/68645
-  [í”„ë¡œê·¸ë˜ë¨¸ìŠ¤][ì›”ê°„ ì½”ë“œ ì±Œë¦°ì§€ ì‹œì¦Œ1] ì‚¼ê° ë‹¬íŒ½ì´ (level 2)
+ * https://programmers.co.kr/learn/courses/30/lessons/68645
+ * ÇÁ·Î±×·¡¸Ó½º - ¿ù°£ ÄÚµå Ã§¸°Áö ½ÃÁğ1 - »ï°¢ ´ŞÆØÀÌ (level 2)
  */
 package week1.question1;
 
 class Solution {
     public int[] solution(int n) {
-        int[] answer = {};
+        
+        //variables
+        int[] answer = new int[(n * (n + 1)) / 2];
+        int[][] pyramid = new int[n][n];
+        
+        int row = -1;
+        int column = 0;
+        int num = 1;
+        int index = 0;
+
+        //from first row, each column goes down
+        //next row is decreased by one, and goes right
+        //then lasts are goes up
+        for(int i = 0; i < pyramid.length; i++)
+        { 
+            for(int j = i; j < pyramid[i].length; j++)
+            {
+                //down
+                if(i % 3 == 0)
+                {
+                    row++;
+                }
+                
+                //right
+                else if(i % 3 == 1)
+                {
+                    column++;
+                }
+                
+                //up
+                else if(i % 3 == 2)
+                {
+                    row--;
+                    column--;
+                }
+                
+                pyramid[row][column] = num++;  
+            }
+        }
+        
+        //int[][] to int[]
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++) 
+            {
+                if(pyramid[i][j] != 0)
+                {
+                    answer[index++] = pyramid[i][j];
+                }
+            }
+        }
+        
         return answer;
     }
 }

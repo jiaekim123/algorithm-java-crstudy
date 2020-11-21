b3d248f5a5bfc0fd69b04b3b0acdb682a77f5018
Lana-Banana <bran0521@gmail.com>
2020-11-21 21:26:49 +0900

    Solve problem.

--- src/main/java/week1/question2/Solution.java
+++ src/main/java/week1/question2/Solution.java
@@ -5,8 +5,39 @@
 package week1.question2;
 
 class Solution {
+
     public int[] solution(int[][] arr) {
-        int[] answer = {};
+        int[] answer = {0, 0};
+        check(0, 0, arr.length, arr, answer);
         return answer;
+    }
+
+    private static void check(int x, int y, int l, int[][] arr, int[] answer) {
+
+        boolean allOne = true;
+        boolean allZero = true;
+        loop:
+        for (int i = x; i < x + l; i++) {
+            for (int j = y; j < y + l; j++) {
+                if (arr[i][j] == 0) allOne = false;
+                if (arr[i][j] == 1) allZero = false;
+                if (!allOne && !allZero) break loop;
+            }
+        }
+        if (allOne) {
+            answer[1]++;
+            return;
+        }
+        if (allZero) {
+            answer[0]++;
+            return;
+        }
+
+        check(x, y, l / 2, arr, answer);
+        check(x, y + l / 2, l / 2, arr, answer);
+        check(x + l / 2, y, l / 2, arr, answer);
+        check(x + l / 2, y + l / 2, l / 2, arr, answer);
+
+
     }
 }

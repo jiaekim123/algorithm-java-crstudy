bde2c0661b835fdcaf15f49495577033a658ea42
Lana-Banana <bran0521@gmail.com>
2020-11-21 23:38:36 +0900

    Solve Problem

--- src/main/java/week1/question3/Solution.java
+++ src/main/java/week1/question3/Solution.java
@@ -6,7 +6,28 @@ package week1.question3;
 
 class Solution {
     public int solution(int[] a) {
-        int answer = 0;
-        return answer;
+        return 2 + check(a);
+    }
+
+    private static int check(int[] a) {
+
+        int count = 0;
+        int l = a[0];
+        int r = a[a.length - 1];
+
+        for (int i = 1; i < a.length - 1; i++) {
+            if (a[i] < l) {
+                count++;
+                l = a[i];
+            }
+            if (a[a.length - 1 - i] < r) {
+                count++;
+                r = a[a.length - 1 - i];
+            }
+        }
+
+        if (l == r) count--;
+
+        return count;
     }
 }

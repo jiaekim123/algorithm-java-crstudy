bcc5e9b7f23a54da5f45d6ed1299b0a15bec0676
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-11-26 11:07:37 +0900

    [week2][question2] 카카오프렌즈 컬러링북 1차 시도 실패

--- src/main/java/week2/question2/Solution.java
+++ src/main/java/week2/question2/Solution.java
@@ -4,14 +4,86 @@
  */
 package week2.question2;
 
-class Solution {
-    public int[] solution(int m, int n, int[][] picture) {
-        int numberOfArea = 0;
-        int maxSizeOfOneArea = 0;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
+class Solution {
+    private final static int NUMBER_OF_AREA = 0;
+    private final static int MAX_SIZE_OF_ONE_AREA = 1;
+    public int[] solution(int m, int n, int[][] picture) {
         int[] answer = new int[2];
-        answer[0] = numberOfArea;
-        answer[1] = maxSizeOfOneArea;
+        boolean[][] visited = new boolean[picture.length][picture[0].length];
+        bfs(picture, visited, answer);
         return answer;
+    }
+
+    private void bfs(int[][] picture, boolean[][] visited, int[] answer){
+        Queue<Point> queue = new LinkedList<>();
+        for (int i = 0; i<picture.length; i++){
+            for (int j = 0; j<picture[0].length; j++){
+                if (picture[i][j]!=0){
+                    queue.offer(new Point( i, j, picture[i][j]));
+                    visited[i][j] = true;
+                    break;
+                }
+            }
+        }
+        while (!queue.isEmpty()){
+            Point point = queue.poll();
+            List<Point> connectList = new ArrayList<>();
+            List<Point> disconnectList = new ArrayList<>();
+            for (Direction direction : Direction.values()){
+                int newX = point.x + direction.x;
+                int newY = point.y + direction.y;
+                if (newX >= 0 && newX < picture.length && newY >= 0 && newY < picture[0].length) {
+                    if (!visited[newX][newY]) {
+                        if (picture[point.x][point.y] != picture[newX][newY]) {
+                            disconnectList.add(new Point(newX, newY, picture[newX][newY]));
+                        } else {
+                            connectList.add(new Point(newX, newY, picture[newX][newY]));
+                        }
+
+                    }
+                }
+            }
+            if (connectList.size()==0){
+                answer[NUMBER_OF_AREA]++;
+            }
+
+            for (Point newPoint : disconnectList){
+                visited[newPoint.x][newPoint.y] = true;
+                queue.offer(newPoint);
+            }
+            for (Point newPoint : connectList){
+                visited[newPoint.x][newPoint.y] = true;
+                queue.offer(newPoint);
+            }
+
+        }
+
+    }
+}
+class Point {
+    int x, y, color;
+    public Point(int x, int y, int color){
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+}
+enum Direction {
+    DOWN(0,-1),
+    RIGHT(1, 0),
+    UP(0,1),
+    LEFT(-1,0);
+
+    int x;
+    int y;
+
+    Direction(int x, int y){
+        this.x = x;
+        this.y = y;
     }
 }

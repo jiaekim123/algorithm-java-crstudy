814c4d5f8ece23f447d834b2c671b3d87197ea6a
Lana-Banana <bran0521@gmail.com>
2020-12-02 23:50:06 +0900

    Solve Problem

--- src/main/java/week2/question2/Solution.java
+++ src/main/java/week2/question2/Solution.java
@@ -4,14 +4,66 @@
  */
 package week2.question2;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Solution {
     public int[] solution(int m, int n, int[][] picture) {
+        int maxSizeOfOneArea = Integer.MIN_VALUE;
         int numberOfArea = 0;
-        int maxSizeOfOneArea = 0;
+
+        boolean[][] visited = new boolean[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (!visited[i][j] && picture[i][j] != 0) {
+                    int size = getArea(i, j, m, n, picture, visited);
+                    numberOfArea++;
+                    maxSizeOfOneArea = Math.max(size, maxSizeOfOneArea);
+                }
+            }
+        }
 
         int[] answer = new int[2];
         answer[0] = numberOfArea;
         answer[1] = maxSizeOfOneArea;
         return answer;
+    }
+
+    private static class XY {
+        int x, y, c;
+
+        XY(int x, int y, int c) {
+            this.x = x;
+            this.y = y;
+            this.c = c;
+        }
+    }
+
+    private int getArea(int x, int y, int m, int n, int[][] picture, boolean[][] visited) {
+
+        int size = 0;
+        int[] moveX = {0, 1};
+        int[] moveY = {1, 0};
+        Queue<XY> que = new LinkedList<>();
+        visited[x][y] = true;
+        que.offer(new XY(x, y, picture[x][y]));
+
+        while (!que.isEmpty()) {
+            XY now = que.poll();
+            size++;
+
+            for (int i = 0; i < 2; i++) {
+                int xx = now.x + moveX[i];
+                int yy = now.y + moveY[i];
+                if (0 <= xx && xx < m && 0 <= yy && yy < n) {
+                    if (!visited[xx][yy] && picture[xx][yy] == now.c) {
+                        visited[xx][yy] = true;
+                        que.offer(new XY(xx, yy, picture[xx][yy]));
+                    }
+                }
+            }
+        }
+        return size;
     }
 }

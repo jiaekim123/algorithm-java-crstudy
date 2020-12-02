4f5eac049925ccb1f631145a7207fc1b6b8df66e
Lana-Banana <bran0521@gmail.com>
2020-12-03 00:11:25 +0900

    TC 추가

--- src/test/java/week2/question2/SolutionTest.java
+++ src/test/java/week2/question2/SolutionTest.java
@@ -8,6 +8,8 @@ class SolutionTest {
 
     @Test
     void solution() {
+        assertArrayEquals(new int[]{2, 6}, new Solution().solution(6, 4, new int[][]{{1, 1, 1, 0}, {1, 1, 1, 0}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 1}}));
         assertArrayEquals(new int[]{4, 5}, new Solution().solution(6, 4, new int[][]{{1, 1, 1, 0}, {1, 2, 2, 0}, {1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3}, {0, 0, 0, 3}}));
+
     }
 }

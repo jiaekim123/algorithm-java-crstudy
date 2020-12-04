5a900e66456117e7261bc9b17167c68a8f2c20f7
Derek <Derek@DEREK>
2020-12-04 18:05:36 +0900

    Solved 조이스틱 problem

--- src/main/java/week3/question3/Solution.java
+++ src/main/java/week3/question3/Solution.java
@@ -1,12 +1,49 @@
 /*
-    https://programmers.co.kr/learn/courses/30/lessons/42860
-    [í”„ë¡œê·¸ë˜ë¨¸ìŠ¤][íƒìš•ë²•] ì¡°ì´ìŠ¤í‹± (level 3)
+ *	https://programmers.co.kr/learn/courses/30/lessons/42860 
+ *	ÇÁ·Î±×·¡¸Ó½º - Å½¿å¹ı - Á¶ÀÌ½ºÆ½ (level 3)
  */
 package week3.question3;
 
 class Solution {
     public int solution(String name) {
+        
+        //variables
         int answer = 0;
-        return answer;
+        int count = name.length() - 1;
+        int next = 0;
+        
+        //loop through given string
+        for(int i = 0; i < name.length(); i++)
+        {
+            answer += countUpDown(name.charAt(i));
+            
+            //find min left and right
+            next = i + 1;
+            
+            while(next < name.length() && name.charAt(next) == 'A')
+            {
+                next++;
+            }
+            
+            count = Math.min(count, (i * 2) + name.length() - next);
+        }
+
+        return answer + count;
+    }
+    
+    //find min counts
+    private int countUpDown(char change)
+    {
+        //variables
+        int index = 0;
+        int reverseIndex = 0;
+        
+        if(change != 'A')
+        {
+            index = change - 'A';
+            reverseIndex = 'Z' + 1 - change;
+        }
+        
+        return Math.min(index, reverseIndex);
     }
 }

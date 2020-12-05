5002ec5b00acf0e2e1f921d01e4500f5a21f86de
Lana-Banana <bran0521@gmail.com>
2020-12-05 14:59:08 +0900

    Solve Problem & Add TC

--- src/main/java/week3/question1/Solution.java
+++ src/main/java/week3/question1/Solution.java
@@ -4,9 +4,41 @@
  */
 package week3.question1;
 
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] citations) {
-        int answer = 0;
-        return answer;
+
+        Arrays.sort(citations);
+
+        return bs(0, citations[citations.length-1], citations);
+    }
+
+    private static int bs(int start, int end, int[] data) {
+
+        int max = 0;
+
+        while (start < end) {
+            int mid = (start + end) / 2;
+            int h = mid;
+
+            // TODO: 개선 필요.
+            int higher = (int) Arrays.stream(data).filter(v -> h <= v).count();
+            int lower = (int) Arrays.stream(data).filter(v -> h > v).count();
+
+            if (check(lower, higher, h)) {
+                start = mid+1;
+                max = Math.max(h, max);
+            } else {
+                end = mid;
+            }
+        }
+
+        return max;
+
+    }
+
+    private static boolean check(int lower, int higher, int h) {
+        return (lower < h && h <= higher);
     }
 }
--- src/test/java/week3/question1/SolutionTest.java
+++ src/test/java/week3/question1/SolutionTest.java
@@ -9,5 +9,8 @@ class SolutionTest {
     @Test
     void solution() {
         assertEquals(3, new Solution().solution(new int[]{3, 0, 6, 1, 5}));
+        assertEquals(4, new Solution().solution(new int[]{5, 5, 5, 5}));
+        assertEquals(2, new Solution().solution(new int[]{22, 42}));
+
     }
 }

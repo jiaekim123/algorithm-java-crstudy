fde0f1507d7590494d6ceabac8367778a6b72608
jiae <37948906+jiaekim123@users.noreply.github.com>
2020-12-06 20:17:07 +0900

    [week4] Week4 문제 추가
    1. [프로그래머스][2018 KAKAO BLIND RECRUITMENT] 비밀지도 (level 1)
    https://programmers.co.kr/learn/courses/30/lessons/17681
    2. [프로그래머스][2018 KAKAO BLIND RECRUITMENT] 뉴스 클러스터링 (level 2)
    https://programmers.co.kr/learn/courses/30/lessons/17677
    3. [프로그래머스][2018 KAKAO BLIND RECRUITMENT] 추석 트래픽 (level 3)
    https://programmers.co.kr/learn/courses/30/lessons/17676

---
+++ src/main/java/week4/question1/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/17681
+    [프로그래머스][2018 KAKAO BLIND RECRUITMENT] 비밀지도 (level 1)
+ */
+package week4.question1;
+
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = {};
+        return answer;
+    }
+}

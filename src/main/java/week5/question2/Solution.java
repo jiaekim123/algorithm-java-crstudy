3a04ee9addd129d775131df60072e8e2dde69fdb
Derek <Derek@DEREK>
2020-12-15 22:41:00 +0900

    Solved 크레인 인형뽑기 게임 problem

--- src/main/java/week5/question2/Solution.java
+++ src/main/java/week5/question2/Solution.java
@@ -4,9 +4,40 @@
  */
 package week5.question2;
 
+//import
+import java.util.Stack;
+
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-        return answer;
-    }
+  public int solution(int[][] board, int[] moves) {
+      
+      //variables
+      int answer = 0;
+      Stack<Integer> picked = new Stack<>();
+      
+      //pick dolls from board
+      for(int select : moves)
+      {
+          for(int i = 0; i < board.length; i++)
+          {
+              if(board[i][select - 1] != 0)
+              {
+                  if(!picked.isEmpty() && picked.peek() == board[i][select - 1])
+                  {
+                      picked.pop();
+                      answer += 2;
+                  }
+                  
+                  else
+                  {
+                      picked.push(board[i][select - 1]);
+                  }
+                  
+                  board[i][select - 1] = 0;
+                  break;
+              }
+          }
+      }
+      
+      return answer;
+  }
 }

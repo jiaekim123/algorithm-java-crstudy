14e05916adcc32c8d97b0fc0b544b87f0016b153
Lana-Banana <bran0521@gmail.com>
2020-12-27 21:16:43 +0900

    Solve problem ( O(NlogN) )

--- src/main/java/week4/question3/Solution.java
+++ src/main/java/week4/question3/Solution.java
@@ -38,29 +38,49 @@ class Solution {
 
         int answer = Integer.MIN_VALUE;
 
-        for(Request r : requests) {
-            answer = Math.max(getRequestTPS(r.start, requests), answer);
-            answer = Math.max(getRequestTPS(r.end, requests), answer);
+//        O(N^2)
+//        for(Request r : requests) {
+//            answer = Math.max(getRequestTPS(r.start, requests), answer);
+//            answer = Math.max(getRequestTPS(r.end, requests), answer);
+//        }
+
+        int idx = 0;
+        for (Request r : requests) {
+            answer = Math.max(getRequestTPS2(r.end, idx, requests), answer);
+            idx++;
         }
 
         return answer;
     }
 
-    private static int getRequestTPS (double start, ArrayList<Request> requests) {
+    /**
+     * O(NlogN)
+     */
+    private static int getRequestTPS2(double requestEnd, int requsetIdx, ArrayList<Request> requests) {
+        // requests 응답완료시간을 기준으로 오름차순 정렬되어 있다.
         int tps = 0;
-
-        double endOfRange = start + 1;
-
-        for(Request r: requests) {
-            if(     (start <= r.end && r.end <= endOfRange)
-                ||  (start <= r.start && r.end <= endOfRange)
-                ||  (start <= r.start && r.start < endOfRange)
-                ||  (r.start < start && endOfRange < r.end)
-            ) tps++;
+        for (int i = requsetIdx; i < requests.size(); i++) {
+            Request compareReq = requests.get(i);
+            if (compareReq.start < requestEnd + 1) tps++;
         }
-
         return tps;
     }
+
+//    private static int getRequestTPS (double start, ArrayList<Request> requests) {
+//        int tps = 0;
+//
+//        double endOfRange = start + 1;
+//
+//        for(Request r: requests) {
+//            if(     (start <= r.end && r.end <= endOfRange)
+//                ||  (start <= r.start && r.end <= endOfRange)
+//                ||  (start <= r.start && r.start < endOfRange)
+//                ||  (r.start < start && endOfRange < r.end)
+//            ) tps++;
+//        }
+//
+//        return tps;
+//    }
 
 
 }

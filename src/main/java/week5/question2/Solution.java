c04f04d51812eb4ffd59b1dd0ba2826d897b0e0c
Lana-Banana <bran0521@gmail.com>
2020-12-28 16:15:34 +0900

    Solve Problem

--- src/main/java/week5/question2/Solution.java
+++ src/main/java/week5/question2/Solution.java
@@ -4,9 +4,29 @@
  */
 package week5.question2;
 
+import java.util.Stack;
+
 class Solution {
     public int solution(int[][] board, int[] moves) {
+
         int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int m : moves) {
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][m - 1] != 0) {
+                    if (!stack.isEmpty() && stack.peek() == board[i][m - 1] ) {
+                        answer += 2;
+                        stack.pop();
+                    } else {
+                        stack.push(board[i][m - 1]);
+                    }
+                    board[i][m - 1] = 0;
+                    break;
+                }
+            }
+
+        }
         return answer;
     }
 }

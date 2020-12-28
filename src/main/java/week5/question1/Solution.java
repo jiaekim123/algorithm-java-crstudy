e6be334df1b70682aa87f188a62f93d5f2106b59
Lana-Banana <bran0521@gmail.com>
2020-12-28 15:10:26 +0900

    Solve Problem

--- src/main/java/week5/question1/Solution.java
+++ src/main/java/week5/question1/Solution.java
@@ -6,7 +6,38 @@ package week5.question1;
 
 class Solution {
     public int solution(String s) {
-        int answer = 0;
+
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int rsize = 1; rsize <= s.length(); rsize++) {
+
+            int count = 0;
+            int startIdx = 0;
+
+            while (startIdx < s.length() && startIdx + rsize < s.length()) {
+                String repeat = s.substring(startIdx, startIdx + rsize);
+                String temp = s.substring(startIdx);
+                int subCount = getRepeatCount(temp, repeat);
+                count += subCount > 1 ? Math.log10(subCount) + 1 + rsize : rsize;
+                startIdx += subCount * rsize;
+            }
+
+            count += s.length() - startIdx;
+
+            answer = Math.min(count, answer);
+        }
+
         return answer;
+    }
+
+
+    private static int getRepeatCount(String s, String repeat) {
+        int count = 0;
+        while (s.startsWith(repeat)) {
+            s = s.substring(repeat.length());
+            count++;
+        }
+        return count;
     }
 }

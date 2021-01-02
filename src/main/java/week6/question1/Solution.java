36bc7d1de0fc9d33824275cc25819c146140ba6c
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-02 12:16:43 +0900

    [week6] week6 문제 추가
    1. https://programmers.co.kr/learn/courses/30/lessons/42888
       [프로그래머스][2019 KAKAO BLIND RECRUITMENT] 오픈채팅방 (level 2)
    
    2. https://programmers.co.kr/learn/courses/30/lessons/42890
       [프로그래머스][2019 KAKAO BLIND RECRUITMENT] 후보키
    
    3. https://programmers.co.kr/learn/courses/30/lessons/42892
       [프로그래머스][2019 KAKAO BLIND RECRUITMENT] 길찾기 게임

---
+++ src/main/java/week6/question1/Solution.java
@@ -0,0 +1,12 @@
+/*
+    https://programmers.co.kr/learn/courses/30/lessons/42888
+    [프로그래머스][2019 KAKAO BLIND RECRUITMENT] 오픈채팅방 (level 2)
+ */
+package week6.question1;
+
+class Solution {
+    public String[] solution(String[] record) {
+        String[] answer = {};
+        return answer;
+    }
+}
---
+++ src/test/java/week6/question2/SolutionTest.java
@@ -0,0 +1,14 @@
+package week6.question2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+
+    @Test
+    void solution() {
+        assertEquals(2, new Solution().solution(new String[][] {{"100","ryan","music","2"}, {"200","apeach","math","2"}, {"300","tube","computer","3"},
+                {"400","con","computer","4"}, {"500","muzi","music","3"}, {"600","apeach","music","2"}}));
+    }
+}

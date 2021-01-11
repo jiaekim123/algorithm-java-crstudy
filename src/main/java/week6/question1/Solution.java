15a52d076cb7c3861b1be2b8d75fdc7b396d1c27
Lana-Banana <bran0521@gmail.com>
2021-01-11 20:49:53 +0900

    Solve problem

--- src/main/java/week6/question1/Solution.java
+++ src/main/java/week6/question1/Solution.java
@@ -4,9 +4,42 @@
  */
 package week6.question1;
 
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 class Solution {
+
+    static final String enter = "님이 들어왔습니다.";
+    static final String leave = "님이 나갔습니다.";
+
     public String[] solution(String[] record) {
-        String[] answer = {};
-        return answer;
+
+        HashMap<String, String> members = new HashMap<>();
+        StringTokenizer st;
+
+        int count = 0;
+        for (String r : record) {
+            st = new StringTokenizer(r);
+            String status = st.nextToken();
+            String id = st.nextToken();
+            if (status.equals("Enter") || status.equals("Change")) {
+                String nickname = st.nextToken();
+                members.put(id, nickname);
+            }
+            if (status.equals("Enter") || status.equals("Leave")) count++;
+        }
+
+
+        String[] chatting = new String[count];
+        int idx = 0;
+        for (String r : record) {
+            st = new StringTokenizer(r);
+            String status = st.nextToken();
+            String id = st.nextToken();
+            if (!status.equals("Change")) chatting[idx++] = members.get(id) + (status.equals("Enter") ? enter : leave);
+        }
+
+        return chatting;
     }
+
 }

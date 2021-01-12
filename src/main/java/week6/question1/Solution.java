b3d7f7fe7e665a6c4be16dccd4188f7d142d0507
Derek <Derek@DEREK>
2021-01-12 20:42:21 +0900

    Solved 오픈채팅방 problem

--- src/main/java/week6/question1/Solution.java
+++ src/main/java/week6/question1/Solution.java
@@ -4,9 +4,65 @@
  */
 package week6.question1;
 
+//import
+import java.util.HashMap;
+
 class Solution {
-    public String[] solution(String[] record) {
-        String[] answer = {};
-        return answer;
-    }
+  public String[] solution(String[] record) {
+      
+      //variables
+      String[] answer;
+      String[] eachStr;
+      int size = 0;
+      int index = 0;
+      HashMap<String, String> userName = new HashMap<>();
+
+      //split by empty space
+      for(String infos : record)
+      {
+          eachStr = infos.split(" ");
+          
+          //Enter
+          if(eachStr[0].equals("Enter"))
+          {
+              size++;
+              userName.put(eachStr[1], eachStr[2]);
+          }
+          
+          //Leave
+          else if(eachStr[0].equals("Leave"))
+          {
+              size++;
+          }
+          
+          //Change
+          else
+          {
+              userName.put(eachStr[1], eachStr[2]);
+          }
+      }
+
+      //number of "Enter" and "Leave" in records
+      answer = new String[size];
+      
+      //save answers
+      for(String infos : record)
+      {
+          eachStr = infos.split(" ");
+          
+          //Enter
+          if(eachStr[0].equals("Enter"))
+          {
+              answer[index++] = userName.get(eachStr[1]) + "님이 들어왔습니다.";
+          }
+          
+          //Leave
+          if(eachStr[0].equals("Leave"))
+          {
+              answer[index++] = userName.get(eachStr[1]) + "님이 나갔습니다.";
+          }
+      }
+      
+      return answer;
+  }
 }

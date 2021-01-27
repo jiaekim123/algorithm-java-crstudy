bcfd266617f6aa4ee7b609afe35dd1f4459b344c
Derek <Derek@DEREK>
2021-01-27 19:55:59 +0900

    Solved 피보나치 수 problem

--- src/main/java/week8/question1/Solution.java
+++ src/main/java/week8/question1/Solution.java
@@ -6,7 +6,28 @@ package week8.question1;
 
 class Solution {
     public int solution(int n) {
+        
+        //variables
         int answer = 0;
+        int n2 = 0;
+        int n1 = 1;
+        
+        //0, 1
+        if(n < 2)
+        {
+            return n == 0 ? 0 : 1;
+        }
+
+        //increase
+        for(int i = 2; i <= n; i++)
+        {
+            answer = (n1 + n2) % 1234567;
+            
+            //update
+            n2 = n1;
+            n1 = answer;
+        }
+        
         return answer;
     }
 }

24511b1334d3365d81b92a50dfe75b46716120c6
Derek <Derek@DEREK>
2021-01-27 21:36:41 +0900

    Solved 이중우선순위 큐 problem

--- src/main/java/week8/question2/Solution.java
+++ src/main/java/week8/question2/Solution.java
@@ -4,9 +4,49 @@
  */
 package week8.question2;
 
+//import
+import java.util.PriorityQueue;
+import java.util.Collections;
+
 class Solution {
-    public int[] solution(String[] operations) {
-        int[] answer = {};
-        return answer;
-    }
+  public int[] solution(String[] operations) {
+      
+      //variables
+      PriorityQueue<Integer> maxQ = new PriorityQueue<>(Collections.reverseOrder());
+      PriorityQueue<Integer> minQ = new PriorityQueue<>();
+      char inst = ' ';
+      int value = 0;
+      
+      //loop
+      for(String info : operations)
+      {
+          //instruction
+          inst = info.charAt(0);
+          
+          //value
+          value = Integer.parseInt(info.substring(2));
+          
+          //insert
+          if(inst == 'I')
+          {
+              maxQ.add(value);
+              minQ.add(value);
+              
+              continue;
+          }
+          
+          //delete max
+          if(value == 1)
+          {
+              minQ.remove(maxQ.poll());
+              continue;
+          }
+          
+          //delete min
+          maxQ.remove(minQ.poll()); 
+      }
+      
+      //if empty 0,0 otherwise max, min
+      return maxQ.isEmpty() ? new int[] {0, 0} : new int[] {maxQ.peek(), minQ.peek()};
+  }
 }

61344027f2f51da4958d2eb426f7379d9fa43d09
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-28 20:57:49 +0900

    [week8][프로그래머스][DFS/BFS] 여행경로

--- src/main/java/week8/question3/Solution.java
+++ src/main/java/week8/question3/Solution.java
@@ -4,9 +4,31 @@
  */
 package week8.question3;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 class Solution {
     public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        return answer;
+        List<String> answer = new ArrayList<>();
+        boolean[] visited = new boolean[tickets.length];
+        dfs(visited, "ICN", tickets, "ICN", 1, answer);
+        Collections.sort(answer);
+        return answer.get(0).split(" ");
+    }
+
+    private void dfs(boolean[] visited, String city, String[][] tickets, String cities, int count, List<String> answer) {
+        if (count == tickets.length + 1) {
+            answer.add(cities);
+            return;
+        }
+        for (int i = 0; i < tickets.length; i++) {
+            if (!visited[i] && tickets[i][0].equals(city)) {
+                visited[i] = true;
+                dfs(visited, tickets[i][1], tickets, cities + " " + tickets[i][1], count + 1, answer);
+                visited[i] = false;
+            }
+        }
+        return;
     }
 }

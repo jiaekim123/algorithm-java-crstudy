91c469084017a0f973d2524937051ac56cca28f5
jiae <37948906+jiaekim123@users.noreply.github.com>
2021-01-28 12:50:14 +0900

    [week8][프로그래머스] 피보나치 수 해결
    [프로그래머스] 피보나치 수
    - https://programmers.co.kr/learn/courses/30/lessons/12945

--- src/main/java/week8/question1/Solution.java
+++ src/main/java/week8/question1/Solution.java
@@ -6,7 +6,12 @@ package week8.question1;
 
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        return answer;
+        int[] nums = new int[n + 1];
+        nums[0] = 0;
+        nums[1] = 1;
+        for (int i = 2; i < n + 1; i++) {
+            nums[i] = (nums[i - 2] + nums[i - 1]) % 1234567;
+        }
+        return nums[n];
     }
 }

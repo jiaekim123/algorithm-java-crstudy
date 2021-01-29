cfe41f090f36f63e61819baf6827b30420a7749a
Derek <Derek@DEREK>
2021-01-30 01:24:59 +0900

    Solved 여행경로 problem

--- src/main/java/week8/question3/Solution.java
+++ src/main/java/week8/question3/Solution.java
@@ -4,9 +4,70 @@
  */
 package week8.question3;
 
+//import
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
 class Solution {
-    public String[] solution(String[][] tickets) {
-        String[] answer = {};
-        return answer;
-    }
+  //global variables
+  static String route = "";
+  static List<String> sorted = new ArrayList<>();
+  static boolean[] visited;
+
+  public String[] solution(String[][] tickets) {
+
+      //variables
+      String[] answer = {};
+      visited = new boolean[tickets.length];
+
+      for(int i = 0; i < tickets.length; i++)
+      {
+          if(tickets[i][0].equals("ICN"))
+          {
+              visited[i] = true;
+              
+              //reset
+              route = tickets[i][0] + ",";
+
+              dfs(tickets, tickets[i][1], 1);
+
+              visited[i] = false;
+          }
+      }
+
+      Collections.sort(sorted);
+
+      answer = sorted.get(0).split(",");
+
+      return answer;
+  }
+
+  //DFS
+  public void dfs(String[][] tickets, String destination, int count)
+  {
+      route += destination + ",";
+
+      if(count == tickets.length)
+      {
+          sorted.add(route);
+
+          return;
+      }
+
+      for(int i = 0; i < tickets.length; i++)
+      {
+          if(destination.equals(tickets[i][0]) && !visited[i])
+          {
+              visited[i] = true;
+
+              dfs(tickets, tickets[i][1], count + 1);
+
+              visited[i] = false;
+          
+              //delete in reverse order till beginning
+              route = route.substring(0, route.length() - 4);
+          }
+      }
+  }
 }
